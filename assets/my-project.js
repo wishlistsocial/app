9962e49ecc6ea753333ea78612cb92eee443a51e
wishlistsocial <94179275+wishlistsocial@users.noreply.github.com>
2022-01-02 17:24:27 +0100

    Update my-project.js

--- assets/my-project.js
+++ assets/my-project.js
@@ -3,4 +3,4 @@ const urlParams = new URLSearchParams(queryString);
 const username = urlParams.get('username')
 const wish = urlParams.get('wish')
 window.location = "wishlist://username:" + username + "&wish:" + wish;
-window.location = "WislistSocial://wishlist.com?username:" + username + "&wish:" + wish;
+window.location = "wishlist://wishlist.com?username:" + username + "&wish:" + wish;
