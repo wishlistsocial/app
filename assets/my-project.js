7e1159d21b355a1ff22f50865e26d89e805dcc2c
wishlistsocial <94179275+wishlistsocial@users.noreply.github.com>
2022-01-02 16:24:47 +0100

    Delete my-project.js

--- assets/my-project.js
+++
@@ -1,5 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const username = urlParams.get('username')
-const wish = urlParams.get('wish')
-window.location = "wishlist://username:" + username + "&wish:" + wish;
