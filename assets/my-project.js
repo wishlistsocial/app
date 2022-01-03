beb413dc8858682afcc55c962b0ecc932500cc60
wishlistsocial <94179275+wishlistsocial@users.noreply.github.com>
2022-01-03 16:05:03 +0100

    Update my-project.js

--- assets/my-project.js
+++ assets/my-project.js
@@ -2,5 +2,5 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const username = urlParams.get('username')
 const wish = urlParams.get('wish')
-window.location = "wishlist://username:" + username + "&wish:" + wish;
-window.location = "wishlist://wishlist.com?username:" + username + "&wish:" + wish;
+window.location = "wishlist://username=" + username + "&wish=" + wish;
+window.location = "wishlist://wishlist.com?username=" + username + "&wish=" + wish;
